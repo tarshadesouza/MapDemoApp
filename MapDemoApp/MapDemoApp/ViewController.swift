94c4e5d929683ac2188aef0e78a2d4e1cfbfbb3a
Tarsha De Souza <tarsha.desouza@talentomobile.com>
2020-07-08 11:18:08 +0200

    initial commit for project

---
+++ MapDemoApp/MapDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapDemoApp
+//
+//  Created by Tarsha De Souza on 08/07/2020.
+//  Copyright © 2020 TarshaDeSouza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
